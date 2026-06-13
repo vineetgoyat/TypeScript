47fa85b154f3d5a9e2842bbc4d3791c42515a93f
Vineet <vineetgoyat1@gmail.com>
2026-06-13 18:42:18 +0530

    lean the use of -any-

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
-let age: number = 50;
-if (age < 50) 
-    age += 10;
+let sales = 123_456_789; // Numeric separator
+let course = 'TypeScript'; // String literal
+let is_published = true; // Boolean literal atomatically no need to tell the type of variable
+let level; // Variable with no initial value, type will be inferred as 'any'
+
+function render(document: any) {
+console.log(document)
+}
